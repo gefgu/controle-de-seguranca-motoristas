d127218e92f381b70b3f75eb3651bb62cb269f3c
Gustavo Santos <gefgu@hotmail.com>
2024-05-06 14:46:46 -0300

    feat: commit changes to test

---
+++ app-caminhao/App.tsx
@@ -0,0 +1,76 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+import MapView, { Marker, enableLatestRenderer } from "react-native-maps";
+
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject,
+  watchPositionAsync,
+  LocationAccuracy,
+} from "expo-location";
+import { useEffect, useRef, useState } from "react";
+
+enableLatestRenderer();
+
+export default function App() {
+  const [location, setLocation] = useState<LocationObject | null>(null);
+
+  const mapRef = useRef<MapView>(null);
+
+  async function requestLocationPermissions() {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+      console.log(currentPosition);
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermissions();
+  }, []);
+
+  useEffect(() => {
+    watchPositionAsync(
+      {
+        accuracy: LocationAccuracy.Highest,
+        timeInterval: 1000,
+        distanceInterval: 1,
+      },
+      (response) => {
+        setLocation(response);
+        mapRef.current?.animateCamera({
+          pitch: 70,
+          center: response.coords,
+        });
+      }
+    );
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {location && (
+        <MapView
+          ref={mapRef}
+          style={styles.map}
+          initialRegion={{
+            latitude: location?.coords.latitude,
+            longitude: location?.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location?.coords.latitude,
+              longitude: location?.coords.longitude,
+            }}
+          />
+        </MapView>
+      )}
+    </View>
+  );
+}
