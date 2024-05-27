71761174edaa46367f31188bf98730cd623027c7
kinpaginpa <sofia.kawazoe@gmail.com>
2024-05-27 10:51:50 -0300

    feat: truck button

--- app-caminhao/app/manager/overview.tsx
+++ app-caminhao/app/manager/overview.tsx
@@ -5,10 +5,9 @@ import TruckCard from "../../components/TruckCard";
 export default function Overview() {
   return (
     <View style={styles.spaced_out_container}>
-      <TruckCard />
-      <TruckCard />
-      <TruckCard />
-      <TruckCard />
+      <TruckCard status={1} />
+      <TruckCard status={2} />
+      <TruckCard status={3} />
     </View>
   );
 }
--- app-caminhao/components/TruckCard.tsx
+++ app-caminhao/components/TruckCard.tsx
@@ -1,4 +1,7 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+
+const truck_icon = require("../../assets/truck_icon.png");
+const sleep_icon = require("../../assets/sleep.png");
 
 const styles = {
   container: {
@@ -7,19 +10,23 @@ const styles = {
     shadowColor: "#000",
     paddingVertical: 8,
     paddingHorizontal: 16,
-    borderRadius: 8,
+    //borderRadius: 8,
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.75,
     shadowRadius: 4,
-    borderColor: "black",
-    borderWidth: 1,
+    //borderColor: "black",
+    //borderWidth: 1,
   },
 };
 
-export default function TruckCard() {
+export default function TruckCard({ status }: { status: number}) {
   return (
     <TouchableOpacity style={styles.container}>
       <Text>Caminhão</Text>
+      {/* {status === 0 && <Image source={require('./images.jpeg')} />} */}
+      {status === 1 && <Image source={require('./1.jpg')} />}
+      {status === 2 && <Image source={require('./2.jpg')} />}
+      {status === 3 && <Image source={require('./3.jpg')} />}
     </TouchableOpacity>
   );
 }
